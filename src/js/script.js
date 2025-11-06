d36ceabe80d8f523303d9e08e24244874b3df0d9
LinaBenmabrouk <lina.ben_mabrouk@edu.univ-fcomte.fr>
2025-11-06 10:55:17 +0100

    exercice 2

--- src/js/script.js
+++ src/js/script.js
@@ -1,2 +1,9 @@
 const AMettreEnRouge = document.querySelector('#a-mettre-en-rouge')
 AMettreEnRouge.style.color = 'red';
+
+const enRougeSuiteAClick = document.querySelector('#en-rouge-suite-a-click')
+enRougeSuiteAClick.addEventListener('click', () => {
+    enRougeSuiteAClick.style.color = 'red';
+});
+
+
